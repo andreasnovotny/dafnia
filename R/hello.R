f6d4ced6a2f57341898f455d90a64c3ee1ca5cb0
Andreas Novotny <andreas.novotny@su.se>
2018-11-15 10:32:49 +0100

    Version 0.1.1

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,20 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' This says hello to any person!
+#'
+#' Here jkhfjklsdfghdfjklhparam
+#'
+#' @param name A string of characters. Preferentiallly a name of a person. (Default="Andreas")
+#' @return Prints Hello name to screen
+#' @examples hello("Malie")
+#' @export
+#'
 
-hello <- function() {
-  print("Hello, world!")
+hello <- function(name="Andreas") {
+  print(paste("Hello", name))
 }
+
+
+
+
+
+
+
